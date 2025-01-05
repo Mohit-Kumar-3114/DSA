b20633c08375baa811ef0ad4a8c605fe9128bf9a
Mohit <mohitahlawat912@gmail.com>
2025-01-05 17:35:11 +0530

    dsa

---
+++ Bit Manipulation/Lec-2/5.cpp
@@ -0,0 +1,65 @@
+// Divide two integers without using multiplication, division and mod operator
+
+#include <bits/stdc++.h>
+using namespace std;  
+
+
+
+
+// Brute O(dividend) and O(1)
+int Divide(int dividend,int divisor){ 
+    if(dividend==divisor) return 1;
+    bool flag=true;
+    if(dividend<0 and divisor>0) flag=false;
+    if(dividend>=0 and divisor<0) flag=false;
+    long long  n=abs(dividend); // if dividend is 2^-31 then abs = 2^31 which is overflow for int, so use long
+    long long  d=abs(divisor); // if divisor is 2^-31 then abs = 2^31 which is overflow for int, so use long
+    long long ans=0;
+    long long sum=0;
+    while(sum+d<=n){
+        ans++;
+        sum+=d;
+    }
+    if(ans>INT_MAX and flag==false) return INT_MIN;
+    if(ans>INT_MAX and flag==true) return INT_MAX;
+    return flag ? ans : -ans; 
+}
+
+
+
+
+// Optimal O((log dividend)^2) and O(1)
+int Divide2(int dividend,int divisor){
+    if(dividend==divisor) return 1;
+    bool flag=true;
+    if(dividend<0 and divisor>0) flag=false;
+    if(dividend>=0 and divisor<0) flag=false;
+    long long  n=abs(dividend); // if dividend is 2^-31 then abs = 2^31 which is overflow for int, so use long
+    long long  d=abs(divisor); // if divisor is 2^-31 then abs = 2^31 which is overflow for int, so use long
+    long long ans=0;
+    while(n>=d){
+        int count=0;
+        while(n>=(d << (count+1))){ // n >= d*2^(count+1)
+        count++;
+        }
+        ans+= (1 << count); // ans+=2^count
+        n=n-(d << count);
+    }
+
+    if(ans>INT_MAX and flag==false) return INT_MIN;
+    if(ans>INT_MAX and flag==true) return INT_MAX;
+    return flag ? ans : -ans; 
+}
+
+
+
+
+int main(){
+    int dividend=-22;
+    int divisor=3;
+    int ans=Divide(dividend,divisor);
+    int ans2=Divide2(dividend,divisor);
+    cout<<ans<<endl;
+    cout<<ans2<<endl;
+    return 0;
+}
