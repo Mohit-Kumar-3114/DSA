52b49766cc5a8d3ff38e14c866eaefaa76df825e
unknown <mohitahlawat912@gmail.com>
2025-01-30 19:01:18 +0530

    dsa

---
+++ Array/Lec-3/9.cpp
@@ -0,0 +1,102 @@
+// Find repeating and missing number
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+// Brute O(n^2) and O(1)
+vector<int> findMissingRepeatingNumbers(vector<int> a) {
+    int n = a.size(); 
+    int repeating = -1, missing = -1;
+    for (int i = 1; i <= n; i++) {
+        int cnt = 0;
+        for (int j = 0; j < n; j++) {
+            if (a[j] == i) cnt++;
+        }
+
+        if (cnt == 2) repeating = i;
+        else if (cnt == 0) missing = i;
+
+        if (repeating != -1 && missing != -1)
+            break;
+    }
+    return {repeating, missing};
+}
+
+
+
+
+// Better O(2n) and O(n) 
+vector<int> findMissingRepeatingNumbers2(vector<int> a) {
+    int n = a.size();
+    int hash[n + 1] = {0}; // hash array
+
+    for (int i = 0; i < n; i++) {
+        hash[a[i]]++;
+    }
+
+    int repeating = -1, missing = -1;
+    for (int i = 1; i <= n; i++) {
+        if (hash[i] == 2) repeating = i;
+        else if (hash[i] == 0) missing = i;
+
+        if (repeating != -1 && missing != -1)
+            break;
+    }
+    return {repeating, missing};
+}
+
+
+
+
+// Optimal O(n) and O(1)
+vector<int> findMissingRepeatingNumbers3(vector<int> a) {
+    long long n = a.size();
+
+    // Find Sn and S2n:
+    long long SN = (n * (n + 1)) / 2;
+    long long S2N = (n * (n + 1) * (2 * n + 1)) / 6;
+
+    // Calculate S and S2:
+    long long S = 0, S2 = 0;
+    for (int i = 0; i < n; i++) {
+        S += a[i];
+        S2 += (long long)a[i] * (long long)a[i];
+    }
+
+    //S-Sn = X-Y:
+    long long val1 = S - SN;
+
+    // S2-S2n = X^2-Y^2:
+    long long val2 = S2 - S2N;
+
+    //Find X+Y = (X^2-Y^2)/(X-Y):
+    val2 = val2 / val1;
+ 
+    // Here, X-Y = val1 and X+Y = val2:
+    long long x = (val1 + val2) / 2;
+    long long y = x - val1;
+
+    return {(int)x, (int)y};
+}
+
+
+
+
+int main(){
+    vector<int> a = {3, 1, 2, 8, 4, 6, 7, 8};
+    vector<int> ans = findMissingRepeatingNumbers(a);
+    cout << "The repeating and missing numbers are: {"
+         << ans[0] << ", " << ans[1] << "}\n";
+    vector<int> ans2 = findMissingRepeatingNumbers2(a);
+    cout << "The repeating and missing numbers are: {"
+         << ans2[0] << ", " << ans2[1] << "}\n";
+    vector<int> ans3 = findMissingRepeatingNumbers3(a);
+    cout << "The repeating and missing numbers are: {"
+         << ans3[0] << ", " << ans3[1] << "}\n";
+    return 0;
+}
+
+
