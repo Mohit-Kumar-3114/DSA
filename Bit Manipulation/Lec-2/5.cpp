9b9cd4917dc370f2d3bca1672be0ecc7d0070551
unknown <mohitahlawat912@gmail.com>
2025-01-30 14:41:16 +0530

    dsa

--- Bit Manipulation/Lec-2/5.cpp
+++ Bit Manipulation/Lec-2/5.cpp
@@ -8,7 +8,9 @@ using namespace std;
 
 // Brute O(dividend) and O(1)
 int Divide(int dividend,int divisor){ 
-    if(dividend==divisor) return 1;
+    if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
+    }
     bool flag=true;
     if(dividend<0 and divisor>0) flag=false;
     if(dividend>=0 and divisor<0) flag=false;
@@ -20,16 +22,17 @@ int Divide(int dividend,int divisor){
         ans++;
         sum+=d;
     }
-    if(ans>INT_MAX and flag==false) return INT_MIN;
-    if(ans>INT_MAX and flag==true) return INT_MAX;
     return flag ? ans : -ans; 
 }
 
 
 
 
-// Optimal O((log dividend)^2) and O(1)
+// Optimal O(log dividend) and O(1)
 int Divide2(int dividend,int divisor){
+    if (dividend == INT_MIN && divisor == -1) {
+            return INT_MAX;
+    }
     if(dividend==divisor) return 1;
     bool flag=true;
     if(dividend<0 and divisor>0) flag=false;
@@ -45,9 +48,6 @@ int Divide2(int dividend,int divisor){
         ans+= (1 << count); // ans+=2^count
         n=n-(d << count);
     }
-
-    if(ans>INT_MAX and flag==false) return INT_MIN;
-    if(ans>INT_MAX and flag==true) return INT_MAX;
     return flag ? ans : -ans; 
 }
 
--- String/Lec-1/7.cpp
+++ String/Lec-1/7.cpp
@@ -21,9 +21,9 @@ bool isAnagram(string s, string t) {
 bool isAnagram2(string s, string t) {
     if (s.length() != t.length()) return false;
     int arr[26] = {0};
-    for (int i = 0; i < s.length(); i++) {
-        arr[s[i] - 'a']++;
-        arr[t[i] - 'a']--;
+    for (int i = 0; i < s.length(); i++) { 
+        arr[s[i] - 'a']++; 
+        arr[t[i] - 'a']--; 
     }
     for (int i = 0; i < 26; i++) {
         if (arr[i] != 0) return false;
