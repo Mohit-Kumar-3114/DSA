e539a90dd2ffdce3b5f009bb592b527e7db803be
unknown <mohitahlawat912@gmail.com>
2025-02-06 19:25:29 +0530

    dsa

---
+++ Linked List/Lec-1/3.cpp
@@ -0,0 +1,165 @@
+// Deleting node in a linked list
+
+#include <bits/stdc++.h>
+using namespace std; 
+
+
+
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    // Constructors
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+// Function to convert an array to a linked list
+Node* convertArr2LL(vector<int>& arr) {
+    if (arr.empty()) return nullptr; 
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+    for (int i = 1; i < arr.size(); i++) {
+        mover->next = new Node(arr[i]);
+        mover = mover->next;
+    }
+    return head;
+}
+
+// Function to calculate length of linked list
+int lengthofLL(Node* head) {
+    int cnt = 0;
+    Node* temp = head;
+    while (temp) {
+        temp = temp->next;
+        cnt++;
+    }
+    return cnt;
+}
+
+// Function to print linked list
+void printLinkedList(Node* head) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " -> ";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+
+
+
+// Optimal O(1) and O(1)
+void deleteFromStart(Node* &head) {
+    if (head == nullptr) return; 
+    else if(head->next==nullptr){
+        delete head;
+        head=nullptr;
+    }
+    else{
+    Node* temp = head;
+    head = head->next;
+    delete temp; 
+    }
+}
+
+
+
+
+// Optimal O(n) and O(1)
+void deleteFromEnd(Node* &head){
+    if (head == nullptr) return; 
+    else if(head->next==nullptr){
+        delete head;
+        head=nullptr;
+        return;
+    }
+    else{
+     Node* temp=head;
+     while(temp->next->next){
+        temp=temp->next;
+     }
+     delete temp->next;
+     temp->next=nullptr;
+    }
+}
+
+
+
+
+// Optimal O(n) and O(1)
+void deleteFromKthPosition(Node* &head, int k){
+    if (head == nullptr) return;
+    int length = lengthofLL(head);
+    if(k==1) deleteFromStart(head);
+    else if(k==length) deleteFromEnd(head);
+    else if(k>length) return;
+    else{
+     Node* temp=head;
+     for(int i=1;i<k-1;i++) temp=temp->next;
+     Node* t=temp->next;
+     temp->next=temp->next->next;
+     delete t;
+    }
+}
+
+
+
+
+// Optimal O(n) and O(1)
+void deleteValue(Node* &head, int element){
+    if(head==nullptr) return;
+    if(head->data==element){
+    Node* x=head;
+    head=head->next;
+    delete x;
+    return;
+    }
+    Node* temp=head;
+    while(temp->next){
+    if(temp->next->data==element){
+    Node* t=temp->next;
+    temp->next=temp->next->next;
+    delete t;
+    return;
+    }
+    temp=temp->next;
+    }
+}
+
+
+
+int main() {
+    vector<int> arr = {2, 5, 8, 7, 9, 11, 14, 16};
+    Node* head = convertArr2LL(arr);
+
+    cout << "Original Linked List: ";
+    printLinkedList(head);
+
+    deleteFromStart(head);
+    cout << "After Deleting First Node: ";
+    printLinkedList(head);
+
+    deleteFromEnd(head);
+    cout<<"After Deleting Last Node: ";
+    printLinkedList(head);
+
+    deleteFromKthPosition(head,4);
+    cout<<"After Deleting Kth Node: ";
+    printLinkedList(head);
+
+    deleteValue(head,7);
+    cout<<"After Deleting Element: ";
+    printLinkedList(head);
+
+    return 0;
+}
