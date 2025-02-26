f8897d530a7c27c408e2f3d6a0c7f9b9c26a4ea5
unknown <mohitahlawat912@gmail.com>
2025-02-26 18:37:23 +0530

    dsa

--- Binary Tree/Lec-2/1.cpp
+++ Binary Tree/Lec-2/1.cpp
@@ -1,2 +1,37 @@
 // Height of a binary tree
 
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+
+
+
+// Optimal O(n) and O(h)
+int height(Node* root){
+    if(!root) return 0;
+    return max(height(root->left),height(root->right))+1;
+}
+
+
+
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->left->right->left = new Node(5);
+    cout<<"Height of tree is : "<<height(root)<<endl;
+    return 0;
+}
