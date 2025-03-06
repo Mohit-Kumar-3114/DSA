db43a742613d98e89b2933550ac3a7c7cc88a8b7
unknown <mohitahlawat912@gmail.com>
2025-03-06 11:39:10 +0530

    dsa

--- Binary Tree/Lec-2/12.cpp
+++ Binary Tree/Lec-2/12.cpp
@@ -16,6 +16,30 @@ struct Node {
 
 
 
+// Optimal O(n) and O(n)
+bool isSymmetric(Node* root) {
+    if (!root) return true; 
+    queue<Node*> q;
+    q.push(root->left);
+    q.push(root->right);
+    while (!q.empty()) {
+        Node* leftNode = q.front();
+        q.pop();
+        Node* rightNode = q.front();
+        q.pop();
+        if (!leftNode && !rightNode) continue;
+        if (!leftNode || !rightNode || leftNode->data != rightNode->data) return false;
+        q.push(leftNode->left);
+        q.push(rightNode->right);
+        q.push(leftNode->right);
+        q.push(rightNode->left);
+    }
+    return true;
+}
+
+
+
+
 // Optimal O(n) and O(h)
 bool isMirror(Node* leftSub, Node* rightSub) {
     if (leftSub == NULL && rightSub == NULL) return true;
@@ -25,7 +49,7 @@ bool isMirror(Node* leftSub, Node* rightSub) {
            isMirror(leftSub->right, rightSub->left);
 }
 
-bool isSymmetric(Node* root) {
+bool isSymmetric2(Node* root) {
     if (!root) return true; 
     return isMirror(root->left, root->right);
 }
@@ -42,6 +66,11 @@ int main() {
     root->right->left = new Node(4);
     root->right->right = new Node(3);
     if (isSymmetric(root)) {
+        cout << "The binary tree is symmetric (mirror of itself)." << endl;
+    } else {
+        cout << "The binary tree is NOT symmetric." << endl;
+    }
+    if (isSymmetric2(root)) {
         cout << "The binary tree is symmetric (mirror of itself)." << endl;
     } else {
         cout << "The binary tree is NOT symmetric." << endl;
