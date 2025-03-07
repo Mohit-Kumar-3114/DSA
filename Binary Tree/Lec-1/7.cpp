2b30a4fff3e281e46f775d3460543bfbcf2d97f1
unknown <mohitahlawat912@gmail.com>
2025-03-07 11:54:24 +0530

    dsa

---
+++ Binary Tree/Lec-1/7.cpp
@@ -0,0 +1,54 @@
+// Mirror image of a binary tree 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+
+
+// Optimal O(n) and O(h)
+void mirror(Node* root) {
+    if (!root) return;
+    swap(root->left, root->right);
+    mirror(root->left);
+    mirror(root->right);
+}
+
+void inorder(Node* root) {
+    if (!root) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+
+
+
+int main() {
+    Node* root = new Node(3);
+    root->left = new Node(5);
+    root->right = new Node(1);
+    root->left->left = new Node(6);
+    root->left->right = new Node(2);
+    root->right->left = new Node(0);
+    root->right->right = new Node(8);
+    root->left->right->left = new Node(7);
+    root->left->right->right = new Node(4);
+    cout << "Inorder before mirroring: ";
+    inorder(root);
+    cout << endl;
+    mirror(root);
+    cout << "Inorder after mirroring: ";
+    inorder(root);
+    cout << endl;
+    return 0;
+}
