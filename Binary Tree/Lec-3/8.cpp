55987ef77a3ca73a88801380a2474b0138256c4d
unknown <mohitahlawat912@gmail.com>
2025-03-15 12:05:23 +0530

    dsa

---
+++ Binary Tree/Lec-3/8.cpp
@@ -0,0 +1,68 @@
+// Construct a binary tree from inorder and preorder traversal
+                   
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+
+struct Node {
+    int val;
+    Node *left;
+    Node *right;
+    Node(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+
+
+
+// Optimal O(n) and O(n)
+Node* buildTree(vector<int>& preorder, int& preIndex, int start, int end, map<int, int>& inMap) {
+    if (start > end) return NULL;
+
+    int rootVal = preorder[preIndex++];
+    Node* root = new Node(rootVal);
+    int mid = inMap[rootVal];
+
+    root->left = buildTree(preorder, preIndex, start, mid - 1, inMap);
+    root->right = buildTree(preorder, preIndex, mid + 1, end, inMap);
+
+    return root;
+}
+
+Node* build(vector<int>& preorder, vector<int>& inorder) {
+    map<int, int> inMap;
+    for (int i = 0; i < inorder.size(); i++) {
+        inMap[inorder[i]] = i;
+    }
+    
+    int preIndex = 0;
+    return buildTree(preorder, preIndex, 0, inorder.size() - 1, inMap);
+}
+
+
+
+
+void printInorder(Node* root){
+    if(!root){
+        return;
+    }
+    printInorder(root->left);
+    cout << root->val << " ";
+    printInorder(root->right);
+}
+
+
+
+
+int main() {
+    vector<int> inorder = {9, 3, 15, 20, 7};
+    vector<int> preorder = {3, 9, 20, 15, 7};
+    Node* root = build(preorder, inorder);
+    cout << "Inorder of Unique Binary Tree Created: "<< endl;
+    printInorder(root);
+    cout << endl;
+    return 0;
+}
+                            
+                        
